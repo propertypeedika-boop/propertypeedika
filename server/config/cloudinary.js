6f80c1f2381182fe6115371bc7cf04053376f851
bijin thomas <bijinthomas@bijins-MacBook-Pro.local>
2025-11-30 17:20:47 +0100

    Fix CloudinaryStorage import for Render deployment

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,5 +1,5 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const CloudinaryStorage = require('multer-storage-cloudinary').CloudinaryStorage;
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
